e95fae0a85ac621f119f18dfa7b4c0c0a403e3c7
Avinandan Giri <avinandan012@gmail.com>
2015-08-19 18:06:47 +0530

    Create RotateArray.java

---
+++ RotateArray.java
@@ -0,0 +1,28 @@
+public class RotateArray
+{
+  public static void main(String[] args)
+  {
+    int rotationFactor = 4;
+    int[] arr = {0,1,2,3,4,5,6,7,8,9};
+  
+    int[] arr2 = new int[arr.length];
+    
+    int count =0;
+    for(int i=0;i<arr.length;i++)
+  	{
+      if(i<(arr.length-rotationFactor)){
+      	arr2[i]=arr[i+rotationFactor];	
+        
+      }
+      else{
+       
+      	arr2[i]=arr[count];
+        ++count;
+       // System.out.println("count : "+count);
+      }
+      
+      System.out.println(arr2[i]);
+  	}
+  
+  }
+}
